c32cc1acafd5c3c8a90c8f43439f0092fc29ef3e
YeDaxia <yeguozhong@yeah.net>
2020-02-21 22:40:43 +0800

    添加部署脚本

---
+++ job-company/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+deployPath=/apps/wolrdelite/static # 服务器发布路径
+sourcePath=~/source/wolrd-elite/erp-web #服务器源码路径
+
+cd ${sourcePath}
+
+# 安装依赖包,并打包
+sudo npm install
+sudo npm run build:prod
+
+# 覆盖发布
+sudo cp -Rf dist/* ${deployPath} 
---
+++ job-server/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+module=apiServer # 打包模块名
+deployPath=~/apps # 服务器发布路径
+sourcePath=~/source/wolrd-elite/job-server #服务器源码路径
+appPropsFile=src/main/resources/application.properties;
+
+cd ${sourcePath}
+
+#修改application.properties
+sed -i 's/local/dev/g' ${appPropsFile}
+
+# 构建jar包
+sudo ./gradlew clean build -x test
+
+# 拷贝到相应目录
+sudo cp build/libs/*.jar ${deployPath}/${module}.jar
+
+# 重启服务
+if [ ! -f "/etc/init.d/${module}" ];then
+  sudo chmod +x ${deployPath}/${module}.jar
+  sudo ln -s ${deployPath}/${module}.jar /etc/init.d/${module}
+fi
+
+sudo /etc/init.d/${module} restart
+
+#恢复application.properties
+sed -i 's/dev/local/g' ${appPropsFile}
