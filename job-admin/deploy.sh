7825a2893c15894845e32582deed7d63e7ffd2f1
YeDaxia <yeguozhong@yeah.net>
2020-03-01 14:15:47 +0800

    添加最新数据库文件

--- job-admin/deploy.sh
+++ job-admin/deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
-deployPath=/apps/bor/static # 服务器发布路径
-sourcePath=~/source/bor-erp/erp-web #服务器源码路径
+deployPath=/apps/worldelite/static-admin # 服务器发布路径
+sourcePath=~/source/world-elite/job-admin #服务器源码路径
 
 cd ${sourcePath}
 
 # 安装依赖包,并打包
 sudo npm install
-sudo npm run build:prod
+sudo npm run build:stage
 
 # 覆盖发布
 sudo cp -Rf dist/* ${deployPath} 
