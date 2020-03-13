89f92a53c559fcc052e568a4d45f04f126e183f2
YeDaxia <yeguozhong@yeah.net>
2020-03-13 10:50:02 +0800

    修改数据库文件

---
+++ job-server/init-server.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# 创建一个sudo账户
+adduser webuser
+usermod -aG sudo webuser
+su webuser
+
+# 禁用root用户
+sudo passwd -l root
+
+# 安装 git
+sudo apt-get install git
+
+# 安装jdk
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+java -version
+
+# 安装nginx
+sudo apt-get install nginx
+
+# 安装mysql
+sudo apt-get install mysql-server
+
+
+
+# 安装redis
+sudo apt-get install redis-server
+
+# 安装npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install v11.0.0
+npm config set registry https://registry.npm.taobao.org
+npm config list
+
+# 创建相关目录，设置相关权限
+sudo mkdir /apps/
+sudo mkdir /apps/worldelite
+sudo mkdir /apps/worldelite/temp
+sudo mkdir /apps/worldelite/logs
+sudo mkdir /apps/worldelite/search-index
+
+mkdir ~/source  #源码目录
+mkdir ~/apps    #程序目录
+
+# 下载源码，构建jar包，启动服务
+
+# 配置服务和开机启动
+
