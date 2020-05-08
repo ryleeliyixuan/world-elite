19fd4406a9bbadaef198d50261619ec25b0b9659
YeDaxia <yeguozhong@yeah.net>
2020-05-08 11:26:55 +0800

    管理员登录cookie改成会话

--- job-admin/src/utils/auth.js
+++ job-admin/src/utils/auth.js
@@ -1,15 +1,21 @@
-import Cookies from 'js-cookie'
+import Cookies from 'universal-cookie';
+ 
+const cookies = new Cookies();
 
 const TokenKey = 'world_elite_admin_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return cookies.get(TokenKey)
 }
 
-export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+export function setToken(token,  rememberFlag) {
+  if(rememberFlag && rememberFlag == 1){
+    cookies.set(TokenKey, token, {maxAge: 864000})
+  }else{
+    cookies.set(TokenKey, token)
+  } 
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return cookies.remove(TokenKey)
 }
