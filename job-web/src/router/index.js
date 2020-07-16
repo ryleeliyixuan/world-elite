3e8808e563b2cce84f07a8558131024fc42967a2
Jason <1067741001@qq.com>
2020-07-16 12:17:16 +0800

    1

--- job-web/src/router/index.js
+++ job-web/src/router/index.js
@@ -7,70 +7,70 @@ const constantRoutes = [
     {
         path: '/',
         component: () => import('@/views/HomePage'),
-        meta: { title: '首页' }
+        meta: {title: '首页'}
     },
     {
         path: '/login',
         component: () => import('@/views/LoginPage'),
-        meta: { title: '登录' }
+        meta: {title: '登录'}
     },
     {
         path: '/register',
         component: () => import('@/views/RegisterPage'),
-        meta: { title: '注册' }
+        meta: {title: '注册'}
     },
     {
         path: '/edit-resume',
         component: () => import('@/views/EditResumePage'),
-        meta: { title: '编辑简历' }
+        meta: {title: '编辑简历'}
     },
     {
         path: '/company/:id',
         name: 'company',
         component: () => import('@/views/CompanyHomePage'),
-        meta: { title: '公司主页' }
+        meta: {title: '公司主页'}
     },
     {
         path: '/job/:id',
         name: 'job',
         component: () => import('@/views/JobPage'),
-        meta: { title: '职位详情' }
+        meta: {title: '职位详情'}
     },
     {
         path: '/resume/:id',
         name: 'resume',
         component: () => import('@/views/ResumePage'),
-        meta: { title: '简历预览' }
+        meta: {title: '简历预览'}
     },
     {
         path: '/job-list',
         component: () => import('@/views/JobListPage'),
-        meta: { title: '职位列表' }
+        meta: {title: '职位列表'}
     },
     {
         path: '/apply-jobs',
         component: () => import('@/views/ApplyJobListPage'),
-        meta: { title: '我的投递' }
+        meta: {title: '我的投递'}
     },
     {
         path: '/favorites',
         component: () => import('@/views/FavoriteListPage'),
-        meta: { title: '我的收藏' }
+        meta: {title: '我的收藏'}
     },
     {
         path: '/my-activities',
         component: () => import('@/views/MyActivityListPage'),
-        meta: { title: '我的活动' }
+        meta: {title: '我的活动'}
     },
     {
         path: '/messages',
         component: () => import('@/views/MessageListPage'),
-        meta: { title: '消息通知' }
+        meta: {title: '消息通知'}
     },
     {
         path: '/activity-list',
         component: () => import('@/views/ActivityListPage'),
-        meta: { title: '活动列表' }
+        meta: {title: '活动列表'}
     },
     {
         path: '/forget-pwd',
@@ -115,8 +115,11 @@ const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-    //mode: 'history', // require service support
-    scrollBehavior: () => ({ y: 0 }),
+    mode: 'history', // require service support
+    scrollBehavior: (to, from, savedPostion) => {
+        if (to.hash)
+            return {selector: to.hash}
+    },
     routes: constantRoutes
 })
 
