7612682552512c8218df580b0c76e340dc9b43d4
Rylee <liyixuan1998@gmail.com>
2020-09-14 15:33:40 -0700

    页面适配

--- job-web/src/views/UserProtocolPage.vue
+++ job-web/src/views/UserProtocolPage.vue
@@ -1,26 +1,43 @@
 <template>
-    <div class="container">
-        <h3 class="text-center">用户协议和隐私政策</h3>
-        <div v-html="content" class="mt-3"></div>
-    </div>
+  <div class="app-container">
+    <h3 class="text-center">用户协议和隐私政策</h3>
+    <div v-html="content" class="mt-3"></div>
+  </div>
 </template>
 
 <script>
+import { getUserProtocol } from "@/api/config_api";
 
-    import {getUserProtocol} from '@/api/config_api'
-
-    export default {
-        name: "ProtocolPage",
-        data() {
-            return {
-                content: ''
-            }
-        },
-        created() {
-            getUserProtocol().then(response => {
-                this.content = response.data;
-                this.$emit("complete");
-            })
-        }
+export default {
+  name: "ProtocolPage",
+  data() {
+    return {
+      content: "",
     };
+  },
+  created() {
+    getUserProtocol().then((response) => {
+      this.content = response.data;
+    });
+  },
+};
 </script>
+
+
+<style scoped lang="scss">
+.app-container {
+  width: 100%;
+  padding: 5%;
+}
+
+@media screen and (max-width: 440px) {
+  .app-container {
+    width: 100%;
+    padding: 5%;
+
+    .text-center {
+      font-size: 24px;
+    }
+  }
+}
+</style>
