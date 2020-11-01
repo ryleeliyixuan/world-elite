d841875d49991f9d198689b387aa9bb7fb0f6b23
Jason <1067741001@qq.com>
2020-11-02 00:16:08 +0800

    补充丢失文件

--- job-web/src/main.js
+++ job-web/src/main.js
@@ -1,14 +1,18 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue} from 'bootstrap-vue'
 import '@/style/app.scss'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import i18n from '@/i18n'
 
-import echarts from "echarts";import storage from "@/utils/storage"
+import echarts from "echarts";
+import storage from "@/utils/storage"
 
 import '@/permission'
 import '@/icons' // icon
@@ -21,7 +25,6 @@ Object.keys(filters).forEach(key => {
 const version = "2.0";
 
 import Cookies from 'universal-cookie';
-
 const cookies = new Cookies();
 
 if (cookies.get("version") !== version) {
@@ -43,6 +46,10 @@ Vue.prototype.$echarts = echarts;
 
 
 const _vm = new Vue({
+    router,
+    store,
+    i18n,
+    render: h => h(App),
 }).$mount('#app')
 
 document._vm_instance = _vm
