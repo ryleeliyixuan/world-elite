f747952d59ce8df678e554b8b4a9237661604993
dengjijie <1980203697@qq.com>
2020-11-18 16:51:23 +0800

    修复保存公司产品不返回图片名的bug

--- job-server/sql/t_company_product.sql
+++ job-server/sql/t_company_product.sql
@@ -15,3 +15,7 @@ COMMENT='企业产品表'
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
+
+ALTER TABLE `t_company_product`
+	CHANGE COLUMN `imageUrl` `image_url` VARCHAR(255) NULL DEFAULT '' COMMENT '图片地址' COLLATE 'utf8mb4_general_ci' AFTER `description`,
+	CHANGE COLUMN `imageName` `image_name` VARCHAR(255) NULL DEFAULT '' COMMENT '图片名' COLLATE 'utf8mb4_general_ci' AFTER `image_url`;
