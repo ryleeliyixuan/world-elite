e77561ebbb52b81b28dde32d353aefacd148fdeb
xiangchao <964120609@qq.com>
2020-11-24 15:13:15 +0800

    字典表新增面试可预约时间项sql

--- job-server/sql/t_dict.sql
+++ job-server/sql/t_dict.sql
@@ -1,3 +1,11 @@
 INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `del_flag`) VALUES (170, '急招', 12, '3', 0);
 INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `del_flag`) VALUES (171, '热招', 12, '4', 0);
 INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `del_flag`) VALUES (172, '内推', 12, '5', 0);
+
+-- Mock中面试官可预约时间使用
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (179, '本月内', 16, '0-30', '2020-11-23 07:37:51', '2020-11-23 07:58:26', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (178, '两周内', 16, '0-14', '2020-11-23 07:37:33', '2020-11-23 07:58:22', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (177, '本周内', 16, '0-7', '2020-11-23 07:37:18', '2020-11-23 07:58:17', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (176, '3~5天内', 16, '3-5', '2020-11-23 07:37:00', '2020-11-23 07:57:50', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (175, '1~3天内', 16, '0-3', '2020-11-23 07:36:17', '2020-11-23 07:58:14', 0);
+
