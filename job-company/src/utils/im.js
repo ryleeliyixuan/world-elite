05a83e52f712665b1662794e7c4de32437c57469
Jason <1067741001@qq.com>
2020-11-24 13:23:55 +0800

    api统一为https, 并且配置 wss 地址

--- job-company/src/utils/im.js
+++ job-company/src/utils/im.js
@@ -25,8 +25,7 @@ let im = {
                 let timer;
 
                 // 打开一个 web socket
-                // this.ws = new WebSocket("wss://im.websocket.test.myworldelite.com/im");
-                this.ws = new WebSocket("wss://api.myworldelite.com/im/ws/");
+                this.ws = new WebSocket(process.env.WS_BASE_API + "/im/ws/");
 
                 this.ws.onopen = () => {
                     // Web Socket 已连接上，使用 send() 方法发送数据
