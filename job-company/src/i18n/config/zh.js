bfdcf6c5e441af1f4414492dee0045dae3b9d74b
xiangchao <964120609@qq.com>
2020-11-26 17:19:52 +0800

    修改注册页面密码提示,消除歧义

--- job-company/src/i18n/config/zh.js
+++ job-company/src/i18n/config/zh.js
@@ -8,7 +8,7 @@ const zh = {
     get_valid_code: '获取验证码',
     disable_valid_code: '秒后重新获取',
     email_block_tip: '收不到邮件？可能在垃圾箱，请把发件地址设置为白名单。',
-    password_rule: '8~20位，包括数字和字母',
+    password_rule: '密码长度8~20位，需同时包含字母和数字，不支持特殊符号',
     agree_to_regist: '同意并注册',
     login: '登录',
     ask_regist_before: '已经有帐号？',
