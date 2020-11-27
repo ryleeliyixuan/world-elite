a7c95262356e30491d18b97cc7a4aced851e2a08
xiangchao <964120609@qq.com>
2020-11-27 22:27:30 +0800

    dict表添加mock使用的行业数据

--- job-server/sql/t_dict.sql
+++ job-server/sql/t_dict.sql
@@ -9,3 +9,9 @@ INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time
 INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (176, '3~5天内', 16, '3-5', '2020-11-23 07:37:00', '2020-11-23 07:57:50', 0);
 INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (175, '1~3天内', 16, '0-3', '2020-11-23 07:36:17', '2020-11-23 07:58:14', 0);
 
+-- Mock中面试官搜索行业选项
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (190, '互联网', 18, '1', '2020-11-27 15:31:23', '2020-11-27 15:31:23', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (191, '金融', 18, '2', '2020-11-27 15:31:36', '2020-11-27 15:31:36', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (192, '咨询', 18, '3', '2020-11-27 15:31:49', '2020-11-27 15:31:49', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (193, '媒体', 18, '4', '2020-11-27 15:32:01', '2020-11-27 15:32:01', 0);
+INSERT INTO `db_worldelite`.`t_dict`(`id`, `name`, `type`, `value`, `create_time`, `update_time`, `del_flag`) VALUES (194, '房地产', 18, '5', '2020-11-27 15:32:17', '2020-11-27 15:32:17', 0);
