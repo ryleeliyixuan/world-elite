9e1cfbe1ed7a837e11e8fbfdd50c48794a2b11a5
tangweize123 <1151207663@qq.com>
2020-12-02 16:01:03 +0800

    用户统计数据后端接口

--- job-server/src/main/java/com/worldelite/job/mapper/UserApplicantMapper.java
+++ job-server/src/main/java/com/worldelite/job/mapper/UserApplicantMapper.java
@@ -4,7 +4,9 @@ import com.worldelite.job.entity.User;
 import com.worldelite.job.entity.UserApplicant;
 import com.worldelite.job.entity.UserOptions;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface UserApplicantMapper {
     /**
@@ -58,4 +60,12 @@ public interface UserApplicantMapper {
     UserApplicant selectByEmail(String email);
 
     List<UserApplicant> selectAndList(UserOptions options);
+
+    List<Map<String, Object>> selectStatInfo(Integer days);
+
+    List<Map<String, Object>> selectSpecifyMonthStatInfo(String date);
+
+    Date selectFirstCreateTime();
+
+    List<Map<String, Object>> selectSpecifyYearStatInfo(String dateStr);
 }
