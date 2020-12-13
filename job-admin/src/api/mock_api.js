b6989311695b96bb9aa4c2cd70179da47559a0af
xiangchao <964120609@qq.com>
2020-12-13 21:50:12 +0800

    添加面试官页面

--- job-admin/src/api/mock_api.js
+++ job-admin/src/api/mock_api.js
@@ -18,3 +18,27 @@ export function getAvatar() {
     method: 'get'
   })
 }
+
+export function addInterview(data) {
+  return request({
+    url: '/mock/interviewer/info',
+    method: 'post',
+    data
+  })
+}
+
+export function addInterviewDirection(data) {
+  return request({
+    url: '/mock/interviewer/direction',
+    method: 'post',
+    data
+  })
+}
+
+export function addInterviewAuth(data) {
+  return request({
+    url: '/mock/interviewer/auth',
+    method: 'post',
+    data
+  })
+}
