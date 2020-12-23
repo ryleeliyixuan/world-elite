cad5fb598b7fdd863832b28b4d2aa3b0ea9cfea8
Jason <1067741001@qq.com>
2020-12-23 20:52:35 +0800

    解决 发帖页面刷新后工具栏置顶

--- job-web/src/components/Tinymce/index.vue
+++ job-web/src/components/Tinymce/index.vue
@@ -90,7 +90,7 @@
           draggable_modal: true, // 模态窗口允许拖动
           // fixed_toolbar_container: '#mytoolbar', // 指定工具栏在某一容器顶部固定。
           toolbar_mode: 'sliding',
-          toolbar_sticky: true, // 粘性工具栏（或停靠工具栏），在向下滚动网页直到不再可见编辑器时，将工具栏和菜单停靠在屏幕顶部。
+          toolbar_sticky: false, // 粘性工具栏（或停靠工具栏），在向下滚动网页直到不再可见编辑器时，将工具栏和菜单停靠在屏幕顶部。
 
           // 集成配置
           auto_focus: true, // 自动获得焦点
