3852feab064519808135ff6932ec56d4a93831cd
xiangchao <964120609@qq.com>
2020-12-24 17:07:49 +0800

    公司wiki表摘要字段修改为2000字符
    (cherry picked from commit 5f0d5f71a73a66c8d1dd652152c79093f6e5a67d)

--- job-server/sql/t_company_wiki.sql
+++ job-server/sql/t_company_wiki.sql
@@ -9,3 +9,6 @@ ALTER TABLE `t_company_wiki`
 	DROP COLUMN `market_enable`,
 	DROP COLUMN `history_enable`,
 	DROP COLUMN `structure_enable`;
+
+ALTER TABLE `db_worldelite`.`t_company_wiki`
+MODIFY COLUMN `summary` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '摘要' AFTER `company_id`;
