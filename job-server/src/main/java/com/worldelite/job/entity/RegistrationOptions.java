fb2d98e066c3a3d7ba7b58d771ba6659c8b3409a
dengjijie <1980203697@qq.com>
2021-01-12 20:42:13 +0800

    报名状态搜索改成单选

--- job-server/src/main/java/com/worldelite/job/entity/RegistrationOptions.java
+++ job-server/src/main/java/com/worldelite/job/entity/RegistrationOptions.java
@@ -9,20 +9,5 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class RegistrationOptions extends Registration {
-
     private String keywords; //关键词
-    private Integer[] statusList; //状态列表
-
-    public String getStatusList(){
-        if(statusList==null) return null;
-        StringBuffer statusListStr = new StringBuffer();
-        for(int i=0;i<statusList.length;i++){
-            statusListStr.append(statusList[i]);
-            if(i<statusList.length-1){
-                statusListStr.append(",");
-            }
-        }
-        return statusListStr.toString();
-    }
-
 }
--- job-server/src/main/java/com/worldelite/job/form/RegistrationListForm.java
+++ job-server/src/main/java/com/worldelite/job/form/RegistrationListForm.java
@@ -1,15 +1,22 @@
 package com.worldelite.job.form;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Data;
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * 活动报名查询表单
  */
 @Data
-public class RegistrationListForm extends PageForm{
+public class RegistrationListForm extends PageForm implements IExportable{
     private Integer activityId; //活动ID
     private Long userId; //活动发布者ID
     private Long registrationUserId; //活动报名者ID
-    private Integer[] statusList; //活动状态列表
+    private Integer status; //活动状态
     private String keywords; //关键词
+
+    @Override
+    public String genExportExcelName() {
+        return DigestUtils.md5Hex("JobList" + JSON.toJSONString(this)) + ".xlsx";
+    }
 }
