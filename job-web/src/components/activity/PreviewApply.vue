29d05c8ff2539b99f625c9e7ff41e994ff69edb2
Jason <1067741001@qq.com>
2021-01-22 22:30:39 +0800

    开发 报名表列表数据绑定，图片附件直接显示图片，其余显示链接。修复活动详情页bug、实现我报名的活动跳转查看
    (cherry picked from commit 93a5d98bb046a07b0c1832e3f12d0777e596a633)

--- job-web/src/components/activity/PreviewApply.vue
+++ job-web/src/components/activity/PreviewApply.vue
@@ -43,8 +43,8 @@
                 <div class="name"><span v-if="apply.professionFlag==='0'">*</span>专业</div>
                 <el-input size="small" class="value" v-model="applyForm.profession"></el-input>
             </div>
-            <div class="form-item" v-if="apply.genderFlag!=='2'">
-                <div class="name"><span v-if="apply.genderFlag==='0'">*</span>学历</div>
+            <div class="form-item" v-if="apply.educationFlag!=='2'">
+                <div class="name"><span v-if="apply.educationFlag==='0'">*</span>学历</div>
                 <div class="value">
                     <div class="value-item" v-for="education in educationList" :key="education.id" @click="applyForm.educationId = education.id">
                         <svg-icon :icon-class="education.id===applyForm.educationId?'apply-table-selected':'apply-table-unselected'" clickable></svg-icon>
@@ -103,7 +103,7 @@
                 </div>
             </div>
         </div>
-        <div v-if="resumeList" class="resume-container">
+        <div class="resume-container" v-if="needResume==='1'">
             <div class="resume-title"><span>*</span>请提交在线简历</div>
             <el-select v-model="applyForm.resumeId" placeholder="选择在线简历版本" size="small" class="resume-select">
                 <el-option v-for="item in resumeList"
@@ -113,10 +113,10 @@
                            :value="item.id">
                 </el-option>
             </el-select>
-            <div class="resume-button-container">
-                <div class="cancel" @click="onCancel">取消</div>
-                <div class="submit" @click="onSubmit">提交</div>
-            </div>
+        </div>
+        <div v-if="submit" class="resume-button-container">
+            <div class="cancel" @click="onCancel">取消</div>
+            <div class="submit" @click="onSubmit">提交</div>
         </div>
         <div v-else class="button-container">
             <div class="cancel" @click="onCancel">关闭</div>
@@ -134,6 +134,9 @@
             visible: {
                 type: Boolean
             },
+            submit: {
+                type: Boolean
+            },
             apply: {
                 type: Object
             },
@@ -142,15 +145,18 @@
             },
             resumeList: {
                 type: Array
+            },
+            needResume: {
+                type: String
             }
         },
         computed: {
             dialogVisible: {
                 get() {
-                    return this.visible
+                    return this.visible;
                 },
                 set(val) {
-                    this.$emit('update:visible', val)
+                    this.$emit('update:visible', val);
                 }
             }
         },
@@ -167,7 +173,7 @@
                     profession: undefined, // 专业
                     educationId: undefined, // 学历Id，通过获取学历列表接口获取
                     resumeId: undefined, // 活动不需要上传简历时可为空，通过获取简历列表接口获取
-                    answerList: [] // 答案列表
+                    answerList: undefined // 答案列表
                 },
 
                 questionFileUpload: {}, // 当前上传附件的题目
@@ -183,6 +189,8 @@
             }
         },
         mounted() {
+            this.applyForm.activityId = this.activityId;
+
             // 加载学历列表
             this.$axios.get('/dict/list', {params: {type: 25, limit: 99}}).then(response => {
                 this.educationList = response.data.list;
@@ -232,7 +240,6 @@
             // 上传附件
             onFileUpload(question) {
                 this.questionFileUpload = question;
-                console.log("上传附件", question);
             },
 
             // 取消
@@ -242,7 +249,79 @@
 
             // 提交
             onSubmit() {
-                // TODO
+                if (this.checkParams()) {
+                    this.applyForm.answerList = this.apply.questionnaireList.map(item => {
+                        let answer = {questionnaireId: undefined, answerContentList: undefined, answerOptionsIdList: undefined};
+                        answer.questionnaireId = item.id;
+                        switch (item.type) {
+                            case '1':
+                                answer.answerContentList = [item.answer];
+                                break;
+                            case '2':
+                                answer.answerOptionsIdList = item.optionsList.filter(option => option.checked).map(option => option.id);
+                                break;
+                            case '3':
+                                answer.answerOptionsIdList = item.optionsList.filter(option => option.checked).map(option => option.id);
+                                break;
+                            case '4':
+                                answer.answerContentList = [item.fileUrl];
+                                break;
+                        }
+                        return answer;
+                    })
+                    this.$axios.post('/registration', this.applyForm).then(() => {
+                        this.$message.success("报名成功");
+                        this.$set(this, 'dialogVisible', false);
+                    })
+                }
+            },
+
+            // 参数检查
+            checkParams() {
+                let message = undefined;
+
+                let index = this.apply.questionnaireList.findIndex(question => {
+                    let result = false;
+                    if (question.mustAnswer === '1') {
+                        switch (question.type) {
+                            case '1':
+                                result = !question.answer;
+                                break;
+                            case '2':
+                            case '3':
+                                result = question.optionsList.filter(q => q.checked===true).length === 0;
+                                break;
+                            case '4':
+                                result = !question.fileUrl;
+                                break;
+                        }
+                    }
+                    return result;
+                });
+
+                if (!this.applyForm.name) {
+                    message = "请输入姓名";
+                } else if (this.apply.genderFlag === '0' && !this.applyForm.gender) {
+                    message = "请选择性别";
+                } else if (this.apply.phoneFlag === '0' && !this.applyForm.phone) {
+                    message = "请输入手机号";
+                } else if (this.apply.emailFlag === '0' && !this.applyForm.email) {
+                    message = "请输入邮箱";
+                } else if (this.apply.schoolFlag === '0' && !this.applyForm.school) {
+                    message = "请输入学校";
+                } else if (this.apply.gradeFlag === '0' && !this.applyForm.grade) {
+                    message = "请输入年级";
+                } else if (this.apply.professionFlag === '0' && !this.applyForm.profession) {
+                    message = "请输入专业";
+                } else if (this.apply.educationFlag === '0' && !this.applyForm.educationId) {
+                    message = "请选择学历";
+                } else if (index >= 0) {
+                    message = "请完成其他必答题";
+                } else if (this.needResume === '1' && !this.applyForm.resumeId) {
+                    message = "请选择在线简历";
+                }
+                !!message && this.$message.warning(message);
+                return !message;
             }
         }
     }
@@ -558,39 +637,39 @@
                     border-radius: 16px;
                 }
             }
+        }
 
-            .resume-button-container {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                margin-top: 34px;
+        .resume-button-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-top: 34px;
 
-                .cancel {
-                    width: 107px;
-                    height: 35px;
-                    background: #FFFFFF;
-                    border-radius: 18px;
-                    border: 1px solid #4895EF;
-                    font-size: 16px;
-                    color: #4895EF;
-                    line-height: 35px;
-                    text-align: center;
-                    cursor: pointer;
-                }
+            .cancel {
+                width: 107px;
+                height: 35px;
+                background: #FFFFFF;
+                border-radius: 18px;
+                border: 1px solid #4895EF;
+                font-size: 16px;
+                color: #4895EF;
+                line-height: 35px;
+                text-align: center;
+                cursor: pointer;
+            }
 
-                .submit {
-                    width: 106px;
-                    height: 35px;
-                    background: #4895EF;
-                    border-radius: 18px;
-                    font-size: 16px;
-                    font-weight: 500;
-                    color: #FFFFFF;
-                    line-height: 35px;
-                    text-align: center;
-                    margin-left: 21px;
-                    cursor: pointer;
-                }
+            .submit {
+                width: 106px;
+                height: 35px;
+                background: #4895EF;
+                border-radius: 18px;
+                font-size: 16px;
+                font-weight: 500;
+                color: #FFFFFF;
+                line-height: 35px;
+                text-align: center;
+                margin-left: 21px;
+                cursor: pointer;
             }
         }
 
