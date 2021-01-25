07bd1dc9962f03f410d09266b267109368edb11e
xiangchao <964120609@qq.com>
2021-01-25 21:37:18 +0800

    回退获取Dict list硬编码排序; 更新活动城市选项

---
+++ job-server/src/main/resources/db/migration/V2__update_dict_type_23.sql
@@ -0,0 +1,10 @@
+delete from `db_worldelite`.`t_dict` where type = 23;
+
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (230, '北京', 23, '110000');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (231, '上海', 23, '310000');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (232, '广州', 23, '440100');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (233, '深圳', 23, '440300');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (234, '杭州', 23, '330100');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (235, '成都', 23, '510100');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (236, '重庆', 23, '500000');
+INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (237, '天津', 23, '120000');
