c78c25c2533c118e0aa16cee6b019d1a5b8b6069
zxx <531042812@qq.com>
2021-01-28 23:16:04 +0800

    简历生产环境bug修复

--- job-web/src/components/ResumeView.vue
+++ job-web/src/components/ResumeView.vue
@@ -2,15 +2,15 @@
   <div class="resume-container" v-if="resumeDetail">
     <div class="resume-box" style="display: flex">
       <div class="avatar-uploader">
-      <el-image v-if="resumeDetail.avatar && resumeDetail.avatar!=''"
-                :src="resumeDetail.avatar"
-                class="avatar" />
-      <i v-else>
-        <span style="width: 100px;height: 100px;font-size: 14px;padding-left: 32px;line-height: 9;">暂无照片</span>
-      </i>
+        <el-image v-if="resumeDetail.avatar && resumeDetail.avatar!=''"
+                  :src="resumeDetail.avatar"
+                  class="avatar" />
+        <i v-else>
+          <span style="width: 100px;height: 100px;font-size: 14px;padding-left: 32px;line-height: 9;">暂无照片</span>
+        </i>
       </div>
       <div>
-<!--        基本信息-->
+        <!--        基本信息-->
         <div class="resume-info" style="display: flex">
           <div class="info-other-row-l">
             <el-row class="info-name" style="width: 300px">{{resumeDetail.name}}</el-row>
@@ -37,8 +37,8 @@
       </div>
     </div>
     <div
-      v-if="resumeDetail.resumeEduList && resumeDetail.resumeEduList!=''"
-      class="resume-box"
+            v-if="resumeDetail.resumeEduList && resumeDetail.resumeEduList!=''"
+            class="resume-box"
     >
       <el-row>
         <div style="width: 540px;margin-right: 75px">
@@ -47,31 +47,31 @@
           <span class="resume-red">*</span>
         </div>
       </el-row>
-          <div class="resume-edu" v-for="resumeEdu in resumeDetail.resumeEduList"
-               :key="resumeEdu.id">
-            <div style="display: flex;padding-top: 13px;padding-left: 13px;">
-            <div class="edu-box-l" style="width: 350px;">
-              <el-row class="edu-school" style="padding-top: 10px;">{{resumeEdu.schoolName}}</el-row>
-              <el-row class="info-other" style="padding-top: 10px;">学历：{{resumeEdu.degree.name}}</el-row>
-              <el-row class="info-other" style="padding-top: 10px;">专业：{{resumeEdu.majorName}}</el-row>
-            </div>
-            <div class="edu-box-m" style="padding-top: 22px">
-              <el-row class="info-other" style="padding-top: 10px;">在校时间：
-                <span style="font-size: 14px;
+      <div class="resume-edu" v-for="resumeEdu in resumeDetail.resumeEduList"
+           :key="resumeEdu.id">
+        <div style="display: flex;padding-top: 13px;padding-left: 13px;">
+          <div class="edu-box-l" style="width: 350px;">
+            <el-row class="edu-school" style="padding-top: 10px;">{{resumeEdu.schoolName}}</el-row>
+            <el-row class="info-other" style="padding-top: 10px;">学历：{{resumeEdu.degree.name}}</el-row>
+            <el-row class="info-other" style="padding-top: 10px;">专业：{{resumeEdu.majorName}}</el-row>
+          </div>
+          <div class="edu-box-m" style="padding-top: 22px">
+            <el-row class="info-other" style="padding-top: 10px;">在校时间：
+              <span style="font-size: 14px;
                              font-family: PingFangSC-Regular, PingFang SC;
                              font-weight: 400;">
                    {{resumeEdu.startTime == resumeEdu.finishTime? '在读': `${resumeEdu.startTime}到${resumeEdu.finishTime}`}}
                 </span>
-              </el-row>
-              <el-row class="info-other" style="padding-top: 10px;">GPA：{{resumeEdu.gpa}}</el-row>
-            </div>
-            </div>
+            </el-row>
+            <el-row class="info-other" style="padding-top: 10px;">GPA：{{resumeEdu.gpa}}</el-row>
           </div>
+        </div>
+      </div>
     </div>
     <div
-      class="resume-box mt-4"
-      style="margin-top: 10px"
-      v-if="resumeDetail.userExpectJob && resumeDetail.userExpectJob!=''"
+            class="resume-box mt-4"
+            style="margin-top: 10px"
+            v-if="resumeDetail.userExpectJob && resumeDetail.userExpectJob!=''"
     >
       <el-row>
         <div style="width: 540px; display: inline-block;margin-right: 75px">
@@ -118,8 +118,8 @@
       </div>
     </div>
     <div
-      class="resume-box mt-4"
-      v-if="resumeDetail.resumeExpList && resumeDetail.resumeExpList!= ''"
+            class="resume-box mt-4"
+            v-if="resumeDetail.resumeExpList && resumeDetail.resumeExpList!= ''"
     >
       <div style="width: 615px">
         <img src="../assets/point.png" style="padding-right: 9px;padding-bottom: 7px">
@@ -145,7 +145,7 @@
               <div style="width: 380px">
                 <span class="resume-box-text-title">行业：</span>
                 <span
-                      class="resume-box-text-data"
+                        class="resume-box-text-data"
                 >{{ resumeExp.industry}}
                 </span>
               </div>
@@ -206,11 +206,11 @@
           <el-row style="width: 615px;display: inline-flex;padding-left: 13px;">
             <div style="width: 386px">
               <span class="info-other">语种{{index+1}}: </span>
-                <span class="info-other">{{language.title}}</span>
+              <span class="info-other">{{language.title}}</span>
             </div>
             <div style="width: 200px">
               <span class="info-other">证书或分数：</span>
-               <span class="info-other">{{language.description}}</span>
+              <span class="info-other">{{language.description}}</span>
             </div>
           </el-row>
         </div>
@@ -278,99 +278,99 @@
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-import { getResumeDetail } from "@/api/resume_api";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { linkName, wholeLink } from "@/utils/common";
+  import { getResumeDetail } from "@/api/resume_api";
+  import { library } from "@fortawesome/fontawesome-svg-core";
+  import { linkName, wholeLink } from "@/utils/common";
 
-import { faMobileAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+  import { faMobileAlt, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faMobileAlt, faEnvelope);
+  library.add(faMobileAlt, faEnvelope);
 
-export default {
-  name: "ResumeView",
-  props: {
-    resumeDetail: {
-      type: Object,
-    },
-  },
-  watch: {
-    resumeDetail:  {
-      handler (n, o) {
-        console.log('子组件中的resumeId值： ' + n.id)
+  export default {
+    name: "ResumeView",
+    props: {
+      resumeDetail: {
+        type: Object,
       },
-      deep: true // 深度监听父组件传过来对象变化
     },
-  },
-  data() {
-    return {
-      resumeId: undefined,
-      resume:undefined
-    };
-  },
-  created() {
+    watch: {
+      resumeDetail:  {
+        handler (n, o) {
+          console.log('子组件中的resumeId值： ' + n.id)
+        },
+        deep: true // 深度监听父组件传过来对象变化
+      },
+    },
+    data() {
+      return {
+        resumeId: undefined,
+        resume:undefined
+      };
+    },
+    created() {
 
-  },
-  methods: {
+    },
+    methods: {
 
-    linkName(link) {
-      return linkName(link);
+      linkName(link) {
+        return linkName(link);
+      },
+      wholeLink(link) {
+        return wholeLink(link);
+      },
     },
-    wholeLink(link) {
-      return wholeLink(link);
-    },
-  },
-};
+  };
 </script>
 
 <style lang="scss">
-.resume-container {
-  font-size: 15px;
+  .resume-container {
+    font-size: 15px;
 
-  .resume-box {
-    padding-left: 62px;
-    padding-top: 18px;
-    .avatar-uploader{
-      border: 1px solid #3F5FF4;
-      border-radius: 5px;
-      color: #8c939d;
-      width: 126px;
-      height: 126px;
-      line-height: 100px;
+    .resume-box {
+      padding-left: 62px;
+      padding-top: 18px;
+      .avatar-uploader{
+        border: 1px solid #3F5FF4;
+        border-radius: 5px;
+        color: #8c939d;
+        width: 126px;
+        height: 126px;
+        line-height: 100px;
 
 
-    }
-    .resume-info{
-      padding-left: 13px;
-      .info-other-row-l {
-        /*margin-left: 20px;*/
-        width: 256px;
-        .info-name {
-          width: 97px;
-          height: 29px;
-          font-size: 21px;
-          font-family: PingFangSC-Medium, PingFang SC;
-          font-weight: 500;
-          color: #333333;
-          line-height: 29px;
+      }
+      .resume-info{
+        padding-left: 13px;
+        .info-other-row-l {
+          /*margin-left: 20px;*/
+          width: 256px;
+          .info-name {
+            width: 97px;
+            height: 29px;
+            font-size: 21px;
+            font-family: PingFangSC-Medium, PingFang SC;
+            font-weight: 500;
+            color: #333333;
+            line-height: 29px;
+          }
+          .info-other {
+            height: 20px;
+            font-size: 14px;
+            font-family: PingFangSC-Medium, PingFang SC;
+            font-weight: 500;
+            color: #333333;
+          }
         }
-        .info-other {
-          height: 20px;
-          font-size: 14px;
-          font-family: PingFangSC-Medium, PingFang SC;
-          font-weight: 500;
-          color: #333333;
+        .info-other-row-m {
+          margin-top: 30px;
+          padding-bottom: 4px;
+          padding-left: 60px;
         }
       }
-      .info-other-row-m {
-        margin-top: 30px;
-        padding-bottom: 4px;
-      padding-left: 60px;
-      }
-    }
       .resume-box-text{
         font-size: 16px;
         font-family: PingFangSC-Medium, PingFang SC;
@@ -402,129 +402,52 @@ export default {
         color: #666666;
         line-height: 20px;
       }
-    .resume-base {
-      width: 117px;
-      height: 25px;
-      font-size: 18px;
-      font-family: PingFangSC-Medium, PingFang SC;
-      font-weight: 500;
-      color: #333333;
-      line-height: 25px;
-      padding-right: 1px;
-      padding-bottom: 13px;
-    }
-    .resume-red {
-      padding-left: 5px;
-      color: #D9001B;
-      font-size: 20px;
-      height: 9px;
-      width: 9px;
-    }
-    .resume-languageinfo {
-      padding-left: 18px;
-      display: inline-flex;
-
-      .resume-introduction {
-        display:inline-block;
-        word-wrap: break-word;
-        width: 548px;
-        font-size: 14px;
-        font-family: PingFangSC-Regular, PingFang SC;
-        font-weight: 400;
-        color: #666666;
-        line-height: 20px;
-      }
-    }
-
-
-
-  }
-  .resume-eduinfo {
-
-
-    .resume-edu1 {
-      width: 616px;
-      padding-top: 13px;
-
-      .info-other {
-        height: 20px;
-        font-size: 14px;
+      .resume-base {
+        width: 117px;
+        height: 25px;
+        font-size: 18px;
         font-family: PingFangSC-Medium, PingFang SC;
         font-weight: 500;
         color: #333333;
+        line-height: 25px;
+        padding-right: 1px;
+        padding-bottom: 13px;
       }
-    }
-
-    .resume-edu {
-      padding-top: 13px;
-      display: block;
-      ::v-deep.el-tag--light {
-        height: 21px;
-        font-size: 12px;
-        font-family: PingFangSC-Medium, PingFang SC;
-        font-weight: 500;
-        color: #FFFFFF;
-        background: #4CC9F0;
-        border-radius: 5px;
-        line-height: 10px;
-        margin: 5px;
+      .resume-red {
+        padding-left: 5px;
+        color: #D9001B;
+        font-size: 20px;
+        height: 9px;
+        width: 9px;
       }
-      .edu-box-l {
-        width: 346px;
-      }
-      .edu-box-l1 {
-        width: 346px;
-      }
-      .edu-box-m1 {
-           margin-top: 24px;
-           margin-right: 20px;
-
-           .info-other {
-             height: auto;
-             font-size: 14px;
-             font-family: PingFangSC-Medium, PingFang SC;
-             font-weight: 500;
-             color: #333333;
-           }
-         }
-      .edu-box {
+      .resume-languageinfo {
+        padding-left: 18px;
         display: inline-flex;
-        .expinfo-other-row {
-          display: inline-flex;
 
-
-          .expinfo-other {
-            width: 70px;
-            font-size: 14px;
-            font-family: PingFangSC-Medium, PingFang SC;
-            font-weight: 500;
-            color: #333333;
-          }
-
-          .expinfo-other-html {
-            display:inline-block;
-            word-wrap: break-word;
-            width: 478px;
-            font-size: 14px;
-            font-family: PingFangSC-Regular, PingFang SC;
-            font-weight: 400;
-            color: #666666;
-            line-height: 20px;
-          }
+        .resume-introduction {
+          display:inline-block;
+          word-wrap: break-word;
+          width: 548px;
+          font-size: 14px;
+          font-family: PingFangSC-Regular, PingFang SC;
+          font-weight: 400;
+          color: #666666;
+          line-height: 20px;
         }
       }
 
 
-      .edu-box-m {
-        margin-top: 24px;
-        width: 200px;
-        margin-right: 20px;
-      }
-      .edu-box-m1 {
-        margin-top: 24px;
-        margin-right: 20px;
-        .info-other{
-          height: auto;
+
+    }
+    .resume-eduinfo {
+
+
+      .resume-edu1 {
+        width: 616px;
+        padding-top: 13px;
+
+        .info-other {
+          height: 20px;
           font-size: 14px;
           font-family: PingFangSC-Medium, PingFang SC;
           font-weight: 500;
@@ -532,68 +455,145 @@ export default {
         }
       }
 
-      .resume-editbox {
-        padding-top: 30px;
+      .resume-edu {
+        padding-top: 13px;
+        display: block;
+        ::v-deep.el-tag--light {
+          height: 21px;
+          font-size: 12px;
+          font-family: PingFangSC-Medium, PingFang SC;
+          font-weight: 500;
+          color: #FFFFFF;
+          background: #4CC9F0;
+          border-radius: 5px;
+          line-height: 10px;
+          margin: 5px;
+        }
+        .edu-box-l {
+          width: 346px;
+        }
+        .edu-box-l1 {
+          width: 346px;
+        }
+        .edu-box-m1 {
+          margin-top: 24px;
+          margin-right: 20px;
+
+          .info-other {
+            height: auto;
+            font-size: 14px;
+            font-family: PingFangSC-Medium, PingFang SC;
+            font-weight: 500;
+            color: #333333;
+          }
+        }
+        .edu-box {
+          display: inline-flex;
+          .expinfo-other-row {
+            display: inline-flex;
+
+
+            .expinfo-other {
+              width: 70px;
+              font-size: 14px;
+              font-family: PingFangSC-Medium, PingFang SC;
+              font-weight: 500;
+              color: #333333;
+            }
+
+            .expinfo-other-html {
+              display:inline-block;
+              word-wrap: break-word;
+              width: 478px;
+              font-size: 14px;
+              font-family: PingFangSC-Regular, PingFang SC;
+              font-weight: 400;
+              color: #666666;
+              line-height: 20px;
+            }
+          }
+        }
+
+
+        .edu-box-m {
+          margin-top: 24px;
+          width: 200px;
+          margin-right: 20px;
+        }
+        .edu-box-m1 {
+          margin-top: 24px;
+          margin-right: 20px;
+          .info-other{
+            height: auto;
+            font-size: 14px;
+            font-family: PingFangSC-Medium, PingFang SC;
+            font-weight: 500;
+            color: #333333;
+          }
+        }
+
+        .resume-editbox {
+          padding-top: 30px;
+        }
       }
     }
-  }
 
 
 
 
-  .title-container {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    .dot {
-      width: 7px;
-      height: 7px;
-      background: #4f69ed;
-      margin-right: 9px;
+    .title-container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      .dot {
+        width: 7px;
+        height: 7px;
+        background: #4f69ed;
+        margin-right: 9px;
+      }
+    }
+    .tag-acct {
+      margin-left: 13px;
+      font-size: 12px;
+      font-family: PingFangSC-Medium, PingFang SC;
+      font-weight: 500;
+      color: #4f69ed;
+      line-height: 17px;
+    }
+
+    /deep/ .el-tag {
+      background: #4cc9f0;
+      border-radius: 5px;
+      border: 0px;
+      font-size: 12px;
+      font-family: PingFangSC-Medium, PingFang SC;
+      font-weight: 500;
+      color: #ffffff;
+      line-height: 17px;
+      padding-top: 7px;
+    }
+    .avatar-uploader .avatar {
+      width: 126px;
+      height: 126px;
+      display: block;
+    }
+    .avatar {
+      width: 126px;
+      height: 126px;
+      border-radius: 5px;
+      border: 1px solid #3F5FF4;
+    }
+    .description-text p {
+      margin-bottom: 5px;
+    }
+    .time-text {
+      font-size: 14px;
+      font-weight: 400;
+    }
+
+    .position-text {
+      font-size: 14px;
+      font-weight: 500;
     }
   }
-  .tag-acct {
-    margin-left: 13px;
-    font-size: 12px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 500;
-    color: #4f69ed;
-    line-height: 17px;
-  }
-
-  /deep/ .el-tag {
-    background: #4cc9f0;
-    border-radius: 5px;
-    border: 0px;
-    font-size: 12px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 500;
-    color: #ffffff;
-    line-height: 17px;
-    padding-top: 7px;
-  }
-  .avatar-uploader .avatar {
-    width: 126px;
-    height: 126px;
-    display: block;
-  }
-  .avatar {
-    width: 126px;
-    height: 126px;
-    border-radius: 5px;
-    border: 1px solid #3F5FF4;
-  }
-  .description-text p {
-    margin-bottom: 5px;
-  }
-  .time-text {
-    font-size: 14px;
-    font-weight: 400;
-  }
-
-  .position-text {
-    font-size: 14px;
-    font-weight: 500;
-  }
-}
 </style>
