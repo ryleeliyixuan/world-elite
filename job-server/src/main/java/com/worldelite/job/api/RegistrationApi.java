d07eca791b677806f1ed37d6ea8973cf2258bb50
dengjijie <1980203697@qq.com>
2021-01-29 11:37:52 +0800

    添加获取我报名的活动列表接口

--- job-server/src/main/java/com/worldelite/job/api/RegistrationApi.java
+++ job-server/src/main/java/com/worldelite/job/api/RegistrationApi.java
@@ -1,13 +1,12 @@
 package com.worldelite.job.api;
 
 import com.worldelite.job.anatation.RequireLogin;
+import com.worldelite.job.constants.UserType;
+import com.worldelite.job.form.ActivityListForm;
 import com.worldelite.job.form.RegistrationForm;
 import com.worldelite.job.form.RegistrationListForm;
 import com.worldelite.job.service.RegistrationService;
-import com.worldelite.job.vo.ApiResult;
-import com.worldelite.job.vo.PageResult;
-import com.worldelite.job.vo.QuestionnaireTemplateWithAnswerVo;
-import com.worldelite.job.vo.RegistrationVo;
+import com.worldelite.job.vo.*;
 import io.github.yedaxia.apidocs.ApiDoc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -83,5 +82,23 @@ public class RegistrationApi {
     public ApiResult setRegistrationInappropriate(@PathVariable Integer id){
         registrationService.setRegistrationInappropriate(id);
         return ApiResult.ok();
+    }
+
+    /**
+     * 当前用户报名活动列表
+     *
+     * @param pageForm 活动查询表单
+     * @return
+     */
+    @RequireLogin
+    @GetMapping("my-registration-activities")
+    @ApiDoc
+    public ApiResult<PageResult<ActivityVo>> getFavoriteActivityList(ActivityListForm pageForm){
+        if(pageForm.getStatus() != null){
+            PageResult pageResult = registrationService.getMyRegistrationActivityByStatus(pageForm);
+            return ApiResult.ok(pageResult);
+        }
+        PageResult pageResult = registrationService.getMyRegistrationActivity(pageForm);
+        return ApiResult.ok(pageResult);
     }
 }
