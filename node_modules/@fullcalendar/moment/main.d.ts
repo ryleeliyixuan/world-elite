da01cb141ab972c2e3738ee68e4ea0ca30ab945c
xiangchao <964120609@qq.com>
2021-02-02 19:29:18 +0800

    revert node_modules

--- node_modules/@fullcalendar/moment/main.d.ts
+++
@@ -1,9 +0,0 @@
-import { CalendarApi, Duration, PluginDef } from '@fullcalendar/common';
-import moment from 'moment';
-
-declare function toMoment(date: Date, calendar: CalendarApi): moment.Moment;
-declare function toMomentDuration(fcDuration: Duration): moment.Duration;
-declare const _default: PluginDef;
-
-export default _default;
-export { toMoment, toMomentDuration };
