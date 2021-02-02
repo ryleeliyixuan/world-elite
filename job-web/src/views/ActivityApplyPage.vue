ec204fdfc9a3f8c341709d54414a849f09e99069
Jason <1067741001@qq.com>
2021-02-02 18:48:00 +0800

    修改 活动报名页bug
    (cherry picked from commit fab2a9a76f7934a2bc25e12490a68fbd35b0a1be)

--- job-web/src/views/ActivityApplyPage.vue
+++ job-web/src/views/ActivityApplyPage.vue
@@ -103,8 +103,8 @@
                             <div class="button-text" @click.stop="onViewResume(item)" v-if="activity.needResume==='1'">查看简历</div>
                         </div>
                         <div class="button-container" style="margin-top: 13px;" v-if="activity.auditType==='0'">
-                            <div class="button button1-1" v-if="item.status === 1" :style="{activity.status!==6?'pointer':'not-allowed'}" @click.stop="onResolve1(item)">通过报名</div>
-                            <div class="button button1-2" v-if="item.status === 1" :style="{activity.status!==6?'pointer':'not-allowed'}" @click.stop="onReject1(item)">不合适</div>
+                            <div class="button button1-1" v-if="item.status === 1" :style="{cursor:activity.status!==6?'pointer':'not-allowed'}" @click.stop="onResolve1(item)">通过报名</div>
+                            <div class="button button1-2" v-if="item.status === 1" :style="{cursor:activity.status!==6?'pointer':'not-allowed'}" @click.stop="onReject1(item)">不合适</div>
                             <div class="button button2-1" style="cursor: default;" v-if="item.status === 2" @click.stop="">已通过</div>
                             <!--                            <div class="button button2-2" v-if="item.status === 2" @click.stop="onReject2(item)">不合适</div>-->
                             <!--                            <div class="button button3-1" v-if="item.status === 3" @click.stop="onResolve3(item)">重新通过</div>-->
