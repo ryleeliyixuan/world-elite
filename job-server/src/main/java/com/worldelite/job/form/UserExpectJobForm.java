ce0f27308807fbf8fdd0a984e426b80f26d2bc99
xiangchao <964120609@qq.com>
2021-02-03 19:21:09 +0800

    用户求职意向期望薪资字段改为string , 保存逗号分割的薪资id
    (cherry picked from commit 520baa3f515f2b2142dfd0240d4bc75101712b37)

--- job-server/src/main/java/com/worldelite/job/form/UserExpectJobForm.java
+++ job-server/src/main/java/com/worldelite/job/form/UserExpectJobForm.java
@@ -1,9 +1,7 @@
 package com.worldelite.job.form;
 
 import lombok.Data;
-
-import javax.print.DocFlavor;
-import javax.validation.constraints.NotEmpty;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author yeguozhong yedaxia.github.com
@@ -11,43 +9,10 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class UserExpectJobForm {
     private String expectCity; //意向城市
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
     private Integer categoryId; //意向职位
-
-    public Integer getSalaryId() {
-        return salaryId;
-    }
-
-    private Integer salaryId;
-
-    public Long getResumeId() {
-        return resumeId;
-    }
-
+    private String salaryId; //期望薪资, 逗号分割可以多个
     private Long resumeId;
-
-    public String getExpectCity() {
-        return expectCity;
-    }
-
-    public String getExpectWorkType() {
-        return expectWorkType;
-    }
-
-    public String getExpectPosition() {
-        return expectPosition;
-    }
-
     private String expectWorkType; // 工作类型
     private String expectPosition; //expect position
-
-    public String getIndustry() {
-        return industry;
-    }
-
     private String industry;
 }
