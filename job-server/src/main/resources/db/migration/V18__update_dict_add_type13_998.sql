577845540644d99a4896ef0102ee044694728b57
xiangchao <964120609@qq.com>
2021-03-02 17:40:16 +0800

    sql脚本更新; dict按照指定的排序字段获取
    (cherry picked from commit 5357a0d83b471ecabd3ac4e15e059659f09e1603)

--- job-server/src/main/resources/db/migration/V18__update_dict_add_type13_998.sql
+++ job-server/src/main/resources/db/migration/V18__update_dict_add_type13_998.sql
@@ -1,1 +1,10 @@
 INSERT INTO `db_worldelite`.`t_dict` (`id`, `name`, `type`, `value`) VALUES (998, '应届', 13, '998');
+
+UPDATE `db_worldelite`.`t_dict` SET `value` = '1' WHERE `id` = 999;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '2' WHERE `id` = 162;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '3' WHERE `id` = 998;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '4' WHERE `id` = 157;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '5' WHERE `id` = 158;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '6' WHERE `id` = 159;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '7' WHERE `id` = 160;
+UPDATE `db_worldelite`.`t_dict` SET `value` = '8' WHERE `id` = 161;
